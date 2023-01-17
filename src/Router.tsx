7cc5a4e94f3f383459dee4fa214122a9a0bbac87
MauricioAires <mauricioaires015@gmail.com>
2023-01-17 08:20:33 -0300

    feat: config routes

---
+++ src/Router.tsx
@@ -0,0 +1,19 @@
+import { Route, Routes } from 'react-router-dom'
+
+import { DefaultLayout } from './layouts/DefaultLayout'
+
+import { CheckoutPage } from './pages/Checkout'
+import { CheckoutSuccessPage } from './pages/CheckoutSuccess'
+import { HomePage } from './pages/Home'
+
+export function Router() {
+  return (
+    <Routes>
+      <Route path="/" element={<DefaultLayout />}>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/checkout" element={<CheckoutPage />} />
+        <Route path="/checkout/success" element={<CheckoutSuccessPage />} />
+      </Route>
+    </Routes>
+  )
+}
