9bc8707de1362c8b10eea6a1ef970540138db900
MauricioAires <mauricioaires015@gmail.com>
2023-01-17 08:20:49 -0300

    feat: add global style

---
+++ src/styles/global.ts
@@ -0,0 +1,28 @@
+import { createGlobalStyle, css } from 'styled-components'
+
+export const GlobalStyle = createGlobalStyle`
+${({ theme }) => css`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  body {
+    background: ${theme.background};
+    color: ${theme['base-text']};
+    -webkit-font-smoothing: antialiased;
+  }
+
+  body,
+  input,
+  textarea,
+  button {
+    ${({ theme }) => css`
+      font-family: 'Roboto', sans-serif;
+      font-size: ${theme.md};
+      font-weight: ${theme['font-regular']};
+    `}
+  }
+`}
+`
