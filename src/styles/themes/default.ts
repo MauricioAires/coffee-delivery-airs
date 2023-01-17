091e2a6cb69b1dd8ad2159ca20711a1f00c84d93
MauricioAires <mauricioaires015@gmail.com>
2023-01-17 00:44:54 -0300

    feat: config theme

---
+++ src/styles/themes/default.ts
@@ -0,0 +1,38 @@
+export const defaultTheme = {
+  white: '#FFF',
+  background: '#fafafa',
+
+  'yellow-dark': '#c47f17',
+  yellow: '#bdac2c',
+  'yellow-light': '#f1e9c9',
+  'purple-dark': '#4b2995',
+  purple: '#8047f8',
+  'purple-light': '#ebe5f9',
+
+  'base-title': '#272221',
+  'base-subtitle': '#403937',
+  'base-text': '#574f4d',
+  'base-label': '#8d8686',
+
+  'base-hover': '#d7d5d5',
+  'base-button': '#e6e5e5',
+  'base-input': '#ededed',
+  'base-card': '#f3f2f2',
+
+  'font-regular': 400,
+  'font-bold': 700,
+  'font-extra-bold': 900,
+
+  'base-line-height': '130%',
+  'large-line-height': '160%',
+
+  xxs: '0.625rem', // 10px
+  xs: '0.75rem', // 12px
+  sm: '0.875rem', // 14px
+  md: '1rem', // 16px
+  lg: '1.125rem', // 18px
+  xl: '1.25rem', // 20px
+  '2xl': '1.5rem', // 24px
+  '3xl': '1.875rem', // 30px
+  '5xl': '3rem', // 48px
+} as const
