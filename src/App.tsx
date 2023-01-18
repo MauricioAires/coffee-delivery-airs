8775bbc046e2247112972095e1bb978500ea7858
MauricioAires <mauricioaires015@gmail.com>
2023-01-18 10:29:56 -0300

    feat: config router and theme provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,17 @@
+import { BrowserRouter } from 'react-router-dom'
+import { ThemeProvider } from 'styled-components'
+import { Router } from './Router'
+import { GlobalStyle } from './styles/global'
+
+import { defaultTheme } from './styles/themes/default'
+
 export function App() {
-  return <h1>Hello World</h1>
+  return (
+    <BrowserRouter>
+      <ThemeProvider theme={defaultTheme}>
+        <Router />
+        <GlobalStyle />
+      </ThemeProvider>
+    </BrowserRouter>
+  )
 }
