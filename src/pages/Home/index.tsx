9f62efd662275f8783e730cd78d75a1a065edaf9
MauricioAires <mauricioaires015@gmail.com>
2023-01-18 10:29:17 -0300

    feat: base home page

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,12 @@
+import { Button } from '../../components/Button'
+import { Intro } from '../../components/Intro'
+
+import * as S from './styles'
+
+export function HomePage() {
+  return (
+    <S.HomeWrapper>
+      <Intro />
+    </S.HomeWrapper>
+  )
+}
---
+++ src/pages/Home/styles.ts
@@ -0,0 +1,6 @@
+import styled from 'styled-components'
+
+export const HomeWrapper = styled.div`
+  max-width: 1220px;
+  margin: 0 auto;
+`
