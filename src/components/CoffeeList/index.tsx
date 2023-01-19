db7c806051123a3f364a6b68614956102c4e26e3
MauricioAires <mauricioaires015@gmail.com>
2023-01-19 09:28:47 -0300

    feat: add card coffee and list coffee

--- src/components/CoffeeList/index.tsx
+++ src/components/CoffeeList/index.tsx
@@ -1,38 +1,149 @@
-import { ShoppingCart } from 'phosphor-react'
-
 import expresso from '../../assets/coffees/expresso.svg'
-import { Counter } from '../Counter'
-import { ShoppingCartButton } from '../ShoppingCartButton'
+
+import { CoffeeCard } from '../CoffeCard'
 
 import * as S from './styles'
 
+type Coffee = {
+  id: number
+  image: string
+  tags: string[]
+  title: string
+  description: string
+  price: number
+}
+
 export function CoffeeList() {
+  const coffees: Coffee[] = [
+    {
+      id: 1,
+      image: expresso,
+      tags: ['Tradicional', 'Com Leite'],
+      title: 'Expresso Tradicional',
+      description: 'O tradicional café feito com água quente e grãos moídos',
+      price: 9.9,
+    },
+    {
+      id: 1,
+      image: expresso,
+      tags: ['Tradicional', 'Com Leite'],
+      title: 'Expresso Tradicional',
+      description: 'O tradicional café feito com água quente e grãos moídos',
+      price: 9.9,
+    },
+    {
+      id: 1,
+      image: expresso,
+      tags: ['Tradicional', 'Com Leite'],
+      title: 'Expresso Tradicional',
+      description: 'O tradicional café feito com água quente e grãos moídos',
+      price: 9.9,
+    },
+    {
+      id: 1,
+      image: expresso,
+      tags: ['Tradicional', 'Com Leite'],
+      title: 'Expresso Tradicional',
+      description: 'O tradicional café feito com água quente e grãos moídos',
+      price: 9.9,
+    },
+    {
+      id: 1,
+      image: expresso,
+      tags: ['Tradicional', 'Com Leite'],
+      title: 'Expresso Tradicional',
+      description: 'O tradicional café feito com água quente e grãos moídos',
+      price: 9.9,
+    },
+    {
+      id: 1,
+      image: expresso,
+      tags: ['Tradicional', 'Com Leite'],
+      title: 'Expresso Tradicional',
+      description: 'O tradicional café feito com água quente e grãos moídos',
+      price: 9.9,
+    },
+    {
+      id: 1,
+      image: expresso,
+      tags: ['Tradicional', 'Com Leite'],
+      title: 'Expresso Tradicional',
+      description: 'O tradicional café feito com água quente e grãos moídos',
+      price: 9.9,
+    },
+    {
+      id: 1,
+      image: expresso,
+      tags: ['Tradicional', 'Com Leite'],
+      title: 'Expresso Tradicional',
+      description: 'O tradicional café feito com água quente e grãos moídos',
+      price: 9.9,
+    },
+    {
+      id: 1,
+      image: expresso,
+      tags: ['Tradicional', 'Com Leite'],
+      title: 'Expresso Tradicional',
+      description: 'O tradicional café feito com água quente e grãos moídos',
+      price: 9.9,
+    },
+    {
+      id: 1,
+      image: expresso,
+      tags: ['Tradicional', 'Com Leite'],
+      title: 'Expresso Tradicional',
+      description: 'O tradicional café feito com água quente e grãos moídos',
+      price: 9.9,
+    },
+    {
+      id: 1,
+      image: expresso,
+      tags: ['Tradicional', 'Com Leite'],
+      title: 'Expresso Tradicional',
+      description: 'O tradicional café feito com água quente e grãos moídos',
+      price: 9.9,
+    },
+    {
+      id: 1,
+      image: expresso,
+      tags: ['Tradicional', 'Com Leite'],
+      title: 'Expresso Tradicional',
+      description: 'O tradicional café feito com água quente e grãos moídos',
+      price: 9.9,
+    },
+    {
+      id: 1,
+      image: expresso,
+      tags: ['Tradicional', 'Com Leite'],
+      title: 'Expresso Tradicional',
+      description: 'O tradicional café feito com água quente e grãos moídos',
+      price: 9.9,
+    },
+    {
+      id: 1,
+      image: expresso,
+      tags: ['Tradicional', 'Com Leite'],
+      title: 'Expresso Tradicional',
+      description: 'O tradicional café feito com água quente e grãos moídos',
+      price: 9.9,
+    },
+  ]
+
   return (
     <S.CoffeeListWrapper>
       <S.Title>Nossos cafés</S.Title>
 
       <S.Content>
-        <S.CoffeeCard>
-          <img src={expresso} alt="café tradicional" />
-
-          <S.Tags>
-            <span>Tradicional</span>
-            <span>com leite</span>
-          </S.Tags>
-
-          <h3>Expresso Tradicional</h3>
-          <p>O tradicional café feito com água quente e grãos moídos</p>
-          <S.CoffeeCardFooter>
-            <S.Price>
-              <span>R$</span> 9,90
-            </S.Price>
-
-            <div>
-              <Counter />
-              <ShoppingCartButton />
-            </div>
-          </S.CoffeeCardFooter>
-        </S.CoffeeCard>
+        {coffees.map((coffee) => (
+          <CoffeeCard
+            key={1}
+            description={coffee.description}
+            image={coffee.image}
+            price={coffee.price}
+            tags={coffee.tags}
+            title={coffee.title}
+          />
+        ))}
       </S.Content>
     </S.CoffeeListWrapper>
   )
--- src/components/CoffeeList/styles.ts
+++ src/components/CoffeeList/styles.ts
@@ -4,6 +4,7 @@ export const CoffeeListWrapper = styled.div`
   max-width: 1220px;
   margin: 0 auto;
   margin-bottom: 4rem;
+  padding: 0 1rem;
 `
 
 export const Title = styled.h2`
@@ -18,104 +19,9 @@ export const Title = styled.h2`
 
 export const Content = styled.div`
   display: flex;
-  /* justify-content: flex-start; */
-  justify-content: center;
+  justify-content: flex-start;
   align-items: center;
   flex-wrap: wrap;
   gap: 2rem;
   padding: 1.875rem 0;
-`
-
-export const CoffeeCard = styled.div`
-  ${({ theme }) => css`
-    width: calc(calc(100% / 4) - 2rem);
-    min-width: 16rem;
-
-    background: ${theme['base-card']};
-    /* width: 16rem; */
-    border-radius: 6px 36px 6px 36px;
-
-    padding: 0 1.5rem 1.25rem 1.25rem;
-
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    flex-direction: column;
-
-    > img {
-      margin-top: -1.25rem;
-      width: 7.5rem;
-      height: 7.5rem;
-      flex-shrink: 0;
-    }
-
-    > h3 {
-      font-family: 'Baloo 2', sans-serif;
-      font-weight: ${theme['base-line-height']};
-      font-size: ${theme.xl};
-      margin-top: 1rem;
-    }
-
-    > p {
-      font-size: ${theme.sm};
-      line-height: ${theme['base-line-height']};
-      color: ${theme['base-label']};
-      text-align: center;
-      margin-top: 0.5rem;
-    }
-  `}
-`
-
-export const Tags = styled.div`
-  ${({ theme }) => css`
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    gap: 0.25rem;
-
-    > span {
-      text-transform: uppercase;
-      color: ${theme['yellow-dark']};
-      background: ${theme['yellow-light']};
-
-      font-weight: ${theme['base-line-height']};
-      font-size: ${theme.xs};
-      font-weight: ${theme['font-bold']};
-
-      padding: 0.25rem 0.5rem;
-      border-radius: 14px;
-
-      margin-top: 1rem;
-    }
-  `}
-`
-
-export const CoffeeCardFooter = styled.div`
-  display: flex;
-  justify-content: space-between;
-  width: 100%;
-  align-items: center;
-
-  margin-top: 2rem;
-
-  > div {
-    display: flex;
-    align-items: center;
-    gap: 0.5rem;
-  }
-`
-
-export const Price = styled.h4`
-  ${({ theme }) => css`
-    font-family: 'Baloo 2', sans-serif;
-    font-weight: ${theme['font-extra-bold']};
-    font-size: ${theme['2xl']};
-    line-height: ${theme['base-line-height']};
-
-    > span {
-      font-family: 'Roboto', sans-serif;
-      font-weight: ${theme['font-regular']};
-      font-size: ${theme.sm};
-    }
-  `}
 `
