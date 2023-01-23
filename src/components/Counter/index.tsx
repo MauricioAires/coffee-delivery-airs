51852c8be1b38960c0feeabbe5b9352222e71f91
MauricioAires <mauricioaires015@gmail.com>
2023-01-23 10:08:21 -0300

    feat: change counter

--- src/components/Counter/index.tsx
+++ src/components/Counter/index.tsx
@@ -1,14 +1,28 @@
 import { Plus, Minus } from 'phosphor-react'
 
 import * as S from './styles'
-export function Counter() {
+
+interface CounterProps {
+  quantity: number
+  changeQuantity: (quantity: number) => void
+}
+
+export function Counter({ quantity, changeQuantity }: CounterProps) {
+  function handleChangeQuantity(value: number) {
+    changeQuantity(value)
+  }
+
   return (
     <S.CounterWrapper>
-      <button type="button">
+      <button onClick={() => handleChangeQuantity(quantity + 1)} type="button">
         <Plus weight="bold" size={14} />
       </button>
-      <span>1</span>
-      <button disabled type="button">
+      <span>{quantity}</span>
+      <button
+        onClick={() => handleChangeQuantity(quantity - 1)}
+        disabled={quantity === 1}
+        type="button"
+      >
         <Minus weight="bold" size={14} />
       </button>
     </S.CounterWrapper>
