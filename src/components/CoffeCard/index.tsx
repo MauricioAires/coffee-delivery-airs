d10ec81d93c299e2c29b08094ae3f6d537ebf1b1
MauricioAires <mauricioaires015@gmail.com>
2023-01-23 10:08:05 -0300

    feat: add to wishlist

--- src/components/CoffeCard/index.tsx
+++ src/components/CoffeCard/index.tsx
@@ -1,23 +1,23 @@
+import { useState } from 'react'
+import { Coffee, useCoffees } from '../../contexts/coffees'
 import { Counter } from '../Counter'
 import { ShoppingCartButton } from '../ShoppingCartButton'
 
 import * as S from './styles'
 
-interface CoffeeCardProps {
-  image: string
-  tags: string[]
-  title: string
-  description: string
-  price: number
-}
+interface CoffeeCardProps extends Omit<Coffee, 'quantityPurchased'> {}
 
 export function CoffeeCard({
+  id,
   image,
   tags,
   title,
   description,
   price,
 }: CoffeeCardProps) {
+  const [quantitySelected, setQuantitySelected] = useState(1)
+  const { addWishlist } = useCoffees()
+
   function formatCurrency(value: number) {
     const result = Intl.NumberFormat('pt-BR', {
       style: 'currency',
@@ -27,9 +27,15 @@ export function CoffeeCard({
 
     return result.replace(/[a-z]{3}/i, '').trim()
   }
+
+  function handleAddWishlist() {
+    addWishlist(id, quantitySelected)
+    setQuantitySelected(1)
+  }
+
   return (
     <S.CoffeeCard>
-      <img src={image} alt="café tradicional" />
+      <img src={image} alt={title} />
 
       <S.Tags>
         {tags.map((tag) => (
@@ -46,8 +52,11 @@ export function CoffeeCard({
         </S.Price>
 
         <div>
-          <Counter />
-          <ShoppingCartButton />
+          <Counter
+            quantity={quantitySelected}
+            changeQuantity={(e) => setQuantitySelected(e)}
+          />
+          <ShoppingCartButton onClick={handleAddWishlist} />
         </div>
       </S.CoffeeCardFooter>
     </S.CoffeeCard>
