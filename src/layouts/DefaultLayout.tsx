72a42afb5a78a1f5d5c08941b3afa5dbde073d64
MauricioAires <mauricioaires015@gmail.com>
2023-01-22 22:16:12 -0300

    feat: add coffees context

--- src/layouts/DefaultLayout.tsx
+++ src/layouts/DefaultLayout.tsx
@@ -1,11 +1,12 @@
 import { Outlet } from 'react-router-dom'
 import { Header } from '../components/Header'
+import { CoffeesProvider } from '../contexts/coffees'
 
 export function DefaultLayout() {
   return (
-    <div>
+    <CoffeesProvider>
       <Header />
       <Outlet />
-    </div>
+    </CoffeesProvider>
   )
 }
