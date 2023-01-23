acfcea5f552ad71df6d70c5067071cd05f680463
MauricioAires <mauricioaires015@gmail.com>
2023-01-23 10:09:26 -0300

    feat: add badged

--- src/components/ShoppingCartButton/index.tsx
+++ src/components/ShoppingCartButton/index.tsx
@@ -4,13 +4,18 @@ import * as S from './styles'
 
 export interface ShoppingCartButtonProps {
   variant?: 'yellow' | 'purple'
+  amountItems?: number
+  onClick?: () => void
 }
 
 export function ShoppingCartButton({
   variant = 'purple',
+  onClick,
+  amountItems = 0,
 }: ShoppingCartButtonProps) {
   return (
-    <S.Cart variant={variant}>
+    <S.Cart onClick={onClick} variant={variant}>
+      {!!amountItems && <S.Badged>{amountItems}</S.Badged>}
       <ShoppingCart size={22} weight="fill" />
     </S.Cart>
   )
