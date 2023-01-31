95bcfc5b71a73775958305fc32bf34ca701c19bf
MauricioAires <mauricioaires015@gmail.com>
2023-01-31 00:57:08 -0300

    feat: add toaster provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import { BrowserRouter } from 'react-router-dom'
 import { ThemeProvider } from 'styled-components'
+import { Toaster } from 'react-hot-toast'
+
 import { Router } from './Router'
 import { GlobalStyle } from './styles/global'
 
@@ -8,6 +10,7 @@ import { defaultTheme } from './styles/themes/default'
 export function App() {
   return (
     <BrowserRouter>
+      <Toaster />
       <ThemeProvider theme={defaultTheme}>
         <Router />
         <GlobalStyle />
