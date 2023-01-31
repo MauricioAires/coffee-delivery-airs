c4c1e4e616239173666c674b8a14933f47777a1d
MauricioAires <mauricioaires015@gmail.com>
2023-01-30 23:22:22 -0300

    feat: button variant

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,9 +1,22 @@
+import { forwardRef, ForwardRefRenderFunction, HTMLAttributes } from 'react'
+
 import * as S from './styles'
 
-interface ButtonProps {
+interface ButtonProps extends HTMLAttributes<HTMLButtonElement> {
   label: string
+  variant?: 'yellow' | 'purple'
+  minimal?: boolean
 }
 
-export function Button({ label }: ButtonProps) {
-  return <S.ButtonWrapper>{label}</S.ButtonWrapper>
+const BaseButton: ForwardRefRenderFunction<HTMLButtonElement, ButtonProps> = (
+  { label, minimal = false, variant = 'yellow', ...rest },
+  ref,
+) => {
+  return (
+    <S.ButtonWrapper ref={ref} minimal={minimal} variant={variant} {...rest}>
+      {label}
+    </S.ButtonWrapper>
+  )
 }
+
+export const Button = forwardRef(BaseButton)
--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -1,7 +1,44 @@
-import styled, { css } from 'styled-components'
+import styled, { css, DefaultTheme } from 'styled-components'
 
-export const ButtonWrapper = styled.button`
-  ${({ theme }) => css`
+const modifiersButtonWrapper = {
+  purple: (theme: DefaultTheme, minimal: boolean) => css`
+    background: ${minimal ? 'transparent' : theme.purple};
+
+    border: 1px solid ${theme.purple};
+
+    ${minimal &&
+    css`
+      color: ${theme.purple};
+    `}
+
+    &:hover {
+      background: ${minimal ? theme.purple : theme['purple-dark']};
+      color: ${theme.white};
+    }
+  `,
+  yellow: (theme: DefaultTheme, minimal: boolean) => css`
+    background: ${minimal ? 'transparent' : theme.yellow};
+
+    border: 1px solid ${theme.yellow};
+
+    ${minimal &&
+    css`
+      color: ${theme.yellow};
+    `}
+
+    &:hover {
+      background: ${theme['yellow-dark']};
+    }
+  `,
+}
+
+interface ButtonWrapperProps {
+  variant: keyof typeof modifiersButtonWrapper
+  minimal: boolean
+}
+
+export const ButtonWrapper = styled.button<ButtonWrapperProps>`
+  ${({ theme, variant, minimal }) => css`
     width: 100%;
 
     display: flex;
@@ -14,18 +51,16 @@ export const ButtonWrapper = styled.button`
 
     border-radius: 6px;
 
-    background: ${theme.yellow};
     color: ${theme.white};
+
     font-weight: ${theme['font-bold']};
     font-size: ${theme.sm};
 
     text-transform: uppercase;
     line-height: ${theme['large-line-height']};
 
-    transition: background ease-in-out 0.2s;
+    transition: background ease-in-out 0.2s, color ease-in-out 0.2s;
 
-    &:hover {
-      background: ${theme['yellow-dark']};
-    }
+    ${!!variant && modifiersButtonWrapper[variant](theme, minimal)}
   `}
 `
