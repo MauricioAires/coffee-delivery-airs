b44a950c2e867cc56a362ef5e1eccd29f0c649b1
MauricioAires <mauricioaires015@gmail.com>
2023-01-30 23:32:43 -0300

    style: height full view

---
+++ src/layouts/DefaultLayout.styles.ts
@@ -0,0 +1,8 @@
+import styled from 'styled-components'
+
+export const DefaultLayoutWrapper = styled.div`
+  min-height: 100vh;
+  display: flex;
+  justify-content: space-between;
+  flex-direction: column;
+`
--- src/layouts/DefaultLayout.tsx
+++ src/layouts/DefaultLayout.tsx
@@ -1,12 +1,18 @@
 import { Outlet } from 'react-router-dom'
+import { Footer } from '../components/Footer'
 import { Header } from '../components/Header'
 import { CoffeesProvider } from '../contexts/coffees'
 
+import * as S from './DefaultLayout.styles'
+
 export function DefaultLayout() {
   return (
-    <CoffeesProvider>
-      <Header />
-      <Outlet />
-    </CoffeesProvider>
+    <S.DefaultLayoutWrapper>
+      <CoffeesProvider>
+        <Header />
+        <Outlet />
+        <Footer />
+      </CoffeesProvider>
+    </S.DefaultLayoutWrapper>
   )
 }
