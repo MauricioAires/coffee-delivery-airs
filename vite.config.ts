b7f7ea8820411ecdfa20bd53d4cf0aa41afe970c
MauricioAires <mauricioaires015@gmail.com>
2023-01-31 00:56:34 -0300

    feat: config vite environment variables

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from 'vite'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default ({ mode }) => {
+  const env = loadEnv(mode, process.cwd())
+
+  const htmlPlugin = () => ({
+    name: 'html-transform',
+    transformIndexHtml: (html: string): string =>
+      html.replace(/%=(.*?)%/g, (match, p1) => env[p1] ?? match),
+  })
+
+  return defineConfig({
+    plugins: [htmlPlugin()],
+    server: {
+      host: true,
+    },
+  })
+}
